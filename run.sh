365755a90d3e79416e3c2311beb7766f2880e1ed
Jing WANG <jingwangian@gmail.com>
2016-08-15 23:43:18 +1000

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /db/github/flight
+cd /db/github/flight/expedia
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/db/pg9.5/run/lib
 
-/db/github/flight/main.py
+/db/github/flight/expedia/main.py
 
